e59370195657b7703b044e8eed9cedef8c0655b1
Edson A <edsongol1@yahoo.com.br>
2022-05-21 21:43:07 -0300

    Finalizando tarefa 3

--- tarefa-dia-3/pratica-1.js
+++ tarefa-dia-3/pratica-1.js
@@ -1,4 +1,12 @@
-let meunome = 'edson'
-let meupais = 'brasil'
-let meuemail = 'edsongol1@yahoo.com.br'
-console.log(meunome, meupais, meuemail)
+let meunome = 'Edson'
+    let meupais = 'brasil'
+        let meuemail = 'edsongol1@yahoo.com.br'
+            let idade = 36
+                let profissao = 'Analista'
+                    let conhecer = 'Europa'
+
+console.log("Olá, " + meunome + "! Somos aqui da agência de turismo Viagem dos Sonhos." 
+,"Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para " + conhecer + 
+". Confirma pra gente alguns dados? A sua idade é " + idade + " anos? Você é " + profissao + "? E o seu email é " + meuemail + "?"
+," Ficamos no aguardo. Muito obrigado e boa viagem! ")
+
